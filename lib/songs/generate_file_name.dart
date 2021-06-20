2b1c0adcdf056cb13fbe09be1a560b371a79d6ac
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2021-06-21 00:48:28 +0200

    Bug fix.

--- lib/songs/generate_file_name.dart
+++ lib/songs/generate_file_name.dart
@@ -2,5 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:harcapp_core/comm_classes/common.dart';
 
 String generateFileName({bool isConf: false, @required String title}){
-  return (isConf?'oc!_':'o!_') + remPolChars(title).replaceAll(' ', '_');
+  return (isConf?'oc!_':'o!_') + remPolChars(title)
+      .replaceAll(' ', '_')
+      .replaceAll(',', '')
+      .replaceAll('.', '')
+      .replaceAll('(', '')
+      .replaceAll(')', '')
+      .replaceAll(':', '')
+      .replaceAll(';', '')
+      .replaceAll('"', '');
 }
