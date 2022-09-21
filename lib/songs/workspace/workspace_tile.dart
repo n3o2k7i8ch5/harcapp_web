2bd9add4c380bd66638a791f4db5ecac0a6b0c00
dan <iwanicki.daniel@gmail.com>
2022-09-22 00:46:46 +0200

    .

--- lib/songs/workspace/workspace_tile.dart
+++ lib/songs/workspace/workspace_tile.dart
@@ -85,7 +85,7 @@ class WorkspaceTileState extends State<WorkspaceTile>{
                           fontSize: Dimen.TEXT_SIZE_BIG
                       ),
                       maxLines: 1,
-                      overflow: TextOverflow.fade
+                      overflow: TextOverflow.clip
                   )
               )
 
@@ -94,7 +94,7 @@ class WorkspaceTileState extends State<WorkspaceTile>{
           subtitle: Text(
               fileName,
               maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+              overflow: TextOverflow.clip,
               style: AppTextStyle(
                   color:
                   fileName==HINT_FILE_NAME || songFileNameDupErrProv.hasDup(song)?
